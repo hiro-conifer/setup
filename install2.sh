99f08a94bb35be1f6ecefdb21deab6113fa162c9
HiroConny <132643478+HiroConny@users.noreply.github.com>
2023-05-19 14:13:58 +0900

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,15 @@
+ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
+hwclock --systohc
+
+sed -i '/# ja_JP.UTF-8 UTF-8/ja_JP.UTF-8 UTF-8/' /etc/locale.gen
+sed -i '/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo KEYMAP=jp106 > /etc/vconsole.conf
+
+echo -n INPUT_Hostname: 
+read　hostnm
+echo ${hostnm} > /etc/hostname
+echo 127.0.0.1 localhost > /etc/hosts
+::1 localhost >> /etc/hosts
+127.0.1.1 ${hostnm}.localdomain ${hostnm} >> /etc/hosts
