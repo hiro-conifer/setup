1a8b9b84db01892494e8e8d3c34f1a0cdaccd4eb
HiroConny <132643478+HiroConny@users.noreply.github.com>
2023-05-19 11:49:50 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,4 +11,8 @@ mkfs.vfat -F 32 ${tgt_disk}1
 mkswap ${tgt_disk}2
 mkfs.ext4 -F ${tgt_disk}3
 
+mount ${tgt_disk}3 /mnt
+mount --mkdir ${tgt_disk}1 /mnt/boot
+# swapon ${tgt_disk}2
+
 #pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware
