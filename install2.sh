6c7ca3f394b6f54ad8bda36314167a6672aa2e0a
HiroConny <132643478+HiroConny@users.noreply.github.com>
2023-05-20 11:06:29 +0900

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,13 +1,15 @@
+#!/bin/sh
 ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
 hwclock --systohc
 
-echo ja_JP.UTF-8 >> /etc/locale.gen
+echo ja_JP.UTF-8 UTF-8 >> /etc/locale.gen
 echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 echo KEYMAP=jp106 > /etc/vconsole.conf
 
-hostnm=hiropc
+echo TYPE_HOSTNAME:
+read hostnm
 echo ${hostnm} > /etc/hostname
 echo 127.0.0.1 localhost > /etc/hosts
 echo ::1 localhost >> /etc/hosts
@@ -23,7 +25,7 @@ echo editor no >> /boot/loader/loader.conf
 echo title Arch Linux >> /boot/loader/entries/arch.conf
 echo linux /vmlinuz-linux-zen >> /boot/loader/entries/arch.conf
 echo initrd /booster-linux-zen.img >> /boot/loader/entries/arch.conf
-blkid -o export /dev/sdb3 | grep ^PARTUUID >> /boot/loader/entries/arch.conf
+echo options root=`blkid -o export /dev/sdb3 | grep ^PARTUUID` rw >> /boot/loader/entries/arch.conf
 
 systemctl enable NetworkManager.service
 systemctl enable fstrim.timer
