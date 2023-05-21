9e29ff2c78974a84129752966116e486082f3a1f
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2023-05-21 10:14:02 +0900

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -1,5 +1,12 @@
-
+#!/bin/sh
+ln -sf ~/dotfiles/.config ~/.config
+ln -sf ~/dotfiles/.local ~/.local
+ln -sf ~/dotfiles/.xprofile ~/.xprofile
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+sudo pacman -S --noconfirm xorg-xwayland qt5-wayland
+sudo pacman -S --noconfirm lightdm lightdm-gtk-greeter
+sudo pacman -S --noconfirm sway swaylock-fancy swayidle swaybg waybar mako
