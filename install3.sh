8b4d3828d5979ca97e4cfe380796b63109a01605
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2023-05-21 18:51:35 +0900

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.config ~/.config
 ln -sf ~/dotfiles/.local ~/.local
-ln -sf ~/dotfiles/.xprofile ~/.xprofile
 
 git clone https://aur.archlinux.org/yay.git
 cd yay
@@ -9,13 +8,15 @@ makepkg -si --noconfirm
 
 yay -S --noconfirm xorg-xwayland qt5-wayland
 
-yay -S --noconfirm lightdm lightdm-webkit2-greeter lightdm-webkit2-theme-reactive
+yay -S --noconfirm lightdm lightdm-webkit2-greeter lightdm-webkit2-theme-aether
 sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf
 sudo sed -i '/debug_mode          = false/s/false/true/' /etc/lightdm/lightdm-webkit2-greeter.conf
-sudo sed -i '/webkit_theme        = antergos/s/antergos/reactive/' /etc/lightdm/lightdm-webkit2-greeter.conf
 sudo systemctl enable lightdm
 
 yay -S --noconfirm swayfx swaylock-fancy swayidle swaybg waybar mako
-sudo cp ~/dotfiles/sway.sh /usr/local/bin/
+sudo mv ~/dotfiles/sway.sh /usr/local/bin/
 sudo chmod 755 /usr/local/bin/sway.sh
-sudo cp ~/dotfiles/sway.desktop /usr/share/wayland-sessions/
+sudo mv ~/dotfiles/sway.desktop /usr/share/wayland-sessions/
+
+yay -S thunar
+rm -rf ~/dotfiles/yay
