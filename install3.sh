6c045be5be2b1be3dffc4754f2ca89c6af53b0c9
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2023-05-21 16:33:20 +0900

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -8,5 +8,14 @@ cd yay
 makepkg -si --noconfirm
 
 yay -S --noconfirm xorg-xwayland qt5-wayland
+
 yay -S --noconfirm lightdm lightdm-webkit2-greeter lightdm-webkit2-theme-reactive
+sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-webkit2-greeter/g' /etc/lightdm/lightdm.conf
+sudo sed -i '/debug_mode          = false/s/false/true/' /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo sed -i '/webkit_theme        = antergos/s/antergos/reactive/' /etc/lightdm/lightdm-webkit2-greeter.conf
+sudo systemctl enable lightdm
+
 yay -S --noconfirm swayfx swaylock-fancy swayidle swaybg waybar mako
+sudo cp ~/dotfiles/sway.sh /usr/local/bin/
+sudo chmod 755 /usr/local/bin/sway.sh
+sudo cp ~/dotfiles/sway.desktop /usr/share/wayland-sessions/
