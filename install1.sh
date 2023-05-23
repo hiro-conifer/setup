7674f7892f13a3812721074133ec362a0c2b6726
Hirokey <132643478+hiro-conifer@users.noreply.github.com>
2023-05-24 08:18:44 +0900

    Update install1.sh
    fixed target partitions :3

--- install1.sh
+++ install1.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
-echo Type Target Disk:
-read tgt_disk
-lsblk | grep ${tgt_disk}
+echo Type Target Partition:
+read tgt_part
+lsblk | grep ${tgt_part}
 if [ $? != 0 ]; then
   echo 'not found disk.'
   exit
 fi
-tgt_disk=/dev/${tgt_disk}
-echo ${tgt_disk} > target_disk
+tgt_disk=/dev/${tgt_part}
+echo ${tgt_part} > target_part
 
 timedatectl set-ntp true
+timedatectl status
 
-sgdisk -Z ${tgt_disk}
-sgdisk -o ${tgt_disk}
-sgdisk -n 1:0:+512M -t 1:ef00 ${tgt_disk}
-sgdisk -n 2:0:+300M -t 2:8200 ${tgt_disk}
-sgdisk -n 3:0: -t 3:8304 ${tgt_disk}
+sgdisk -Z ${tgt_part}
+sgdisk -o ${tgt_part}
+sgdisk -n 1:0:+300M -t 1:ef00 ${tgt_part}
+sgdisk -n 2:0:+512M -t 2:8200 ${tgt_part}
+sgdisk -n 3:0: -t 3:8304 ${tgt_part}
 
-mkfs.vfat -F 32 ${tgt_disk}1
-mkswap ${tgt_disk}2
-mkfs.ext4 -F ${tgt_disk}3
 
-mount ${tgt_disk}3 /mnt
-swapon ${tgt_disk}2
-mount --mkdir ${tgt_disk}1 /mnt/boot
+mkfs.vfat -F 32 ${tgt_part}1
+mkswap ${tgt_part}2
+mkfs.ext4 -F ${tgt_part}3
+
+mount ${tgt_part}3 /mnt
+swapon ${tgt_part}2
+mount --mkdir ${tgt_part}1 /mnt/boot
 
 pacstrap /mnt base base-devel linux-zen linux-zen-headers linux-firmware git neovim
 
